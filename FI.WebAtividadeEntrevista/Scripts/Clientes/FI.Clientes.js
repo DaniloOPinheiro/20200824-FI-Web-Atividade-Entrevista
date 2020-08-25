2da58bc085488ec7022ae3c6831db916c33cabe0
Danilo.O.Pinheiro <52793184+DaniloOPinheiro@users.noreply.github.com>
2020-08-25 16:18:50 -0300

    Update FI.Clientes.js

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -1,5 +1,11 @@
 ﻿
 $(document).ready(function () {
+    
+    $("#CPF").inputmask({
+        mask: ['999.999.999-99'],
+        keepStatic: true
+    });
+    
     $('#formCadastro').submit(function (e) {
         e.preventDefault();
         $.ajax({
@@ -55,26 +61,4 @@ function ModalDialog(titulo, texto) {
 
     $('body').append(texto);
     $('#' + random).modal('show');
-}
-
-function validaCPF(s) {
-    var i;
-    var l = '';
-    for (i = 0; i < s.length; i++) if (!isNaN(s.charAt(i))) l += s.charAt(i);
-    s = l;
-    if (s.length != 11) return false;
-    var c = s.substr(0, 9);
-    var dv = s.substr(9, 2);
-    var d1 = 0;
-    for (i = 0; i < 9; i++) d1 += c.charAt(i) * (10 - i);
-    if (d1 == 0) return false;
-    d1 = 11 - (d1 % 11);
-    if (d1 > 9) d1 = 0;
-    if (dv.charAt(0) != d1) return false;
-    d1 *= 2;
-    for (i = 0; i < 9; i++) d1 += c.charAt(i) * (11 - i)
-    d1 = 11 - (d1 % 11);
-    if (d1 > 9) d1 = 0;
-    if (dv.charAt(1) != d1) return false;
-    return true;
 }
